8f68d766b2dc7bb4fa429f4b886153c8df4d48e5
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-11-22 20:00:18 +0200

    Merge conflict resolved

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import { useState } from "react";
-=======
-import React, { useState } from "react";
->>>>>>> c4f3df6db72cde81fa94afb116801004a0489691
 import axios from "../utils/axios";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-<<<<<<< HEAD
   const [name, setName] = useState("");
-=======
-  const [email, setEmail] = useState("");
->>>>>>> c4f3df6db72cde81fa94afb116801004a0489691
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
@@ -19,12 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-<<<<<<< HEAD
       await axios.post("/register", { name, password });
-=======
-      const response = await axios.post("/register", { email, password });
-      localStorage.setItem("token", response.data.token);
->>>>>>> c4f3df6db72cde81fa94afb116801004a0489691
       navigate("/");
     } catch (error) {
       setError(error.response.data);
@@ -36,17 +23,10 @@ const Register = () => {
       <h2>Register</h2>
       <form onSubmit={handleRegister}>
         <input
-<<<<<<< HEAD
           type="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Name"
-=======
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
->>>>>>> c4f3df6db72cde81fa94afb116801004a0489691
           required
         />
         <input
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,18 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:5000/api",
-<<<<<<< HEAD
   withCredentials: true,
-=======
-});
-
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
->>>>>>> c4f3df6db72cde81fa94afb116801004a0489691
 });
 
 export default instance;
